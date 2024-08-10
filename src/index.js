c1431fd307d4a3f7b3670531c0501b9c7bed6eb0
SaPh <sarahphang1@gmail.com>
2024-08-10 15:02:24 +0200

    Connected JS for forecast data with API

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -51,24 +53,42 @@ function processSearch(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "4d43a96bf55oeb0a7t3fd3d430d45fb3";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiURL).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastHTML = "";
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
 <div class="weather-forecast-day">
- <div class="weather-forecast-date">${day}</div>
- <div class="weather-forecast-icon">☀️</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+ <div>
+  <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+ </div>
  <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
-   <strong>29°</strong>
+   <strong>${Math.round(day.temperature.maximum)}°</strong>
   </div>
-  <div class="weather-forecast-temperature">12°</div>
+  <div class="weather-forecast-temperature">${Math.round(
+    day.temperature.minimum
+  )}°</div>
  </div> 
 </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -77,5 +97,3 @@ function displayForecast() {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", processSearch);
-
-displayForecast();
